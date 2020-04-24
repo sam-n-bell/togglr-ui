d4c22b6a755d059eea22b45b2c553690bf686aef
sambell <bell.sam@Outlook.com>
2020-04-24 10:36:26 -0500

    setting authorizatin header string as constant

--- middleware/auth.js
+++ middleware/auth.js
@@ -35,7 +35,7 @@ export default function(context) {
         const oauthBearer = Cookie.get(constants.systemConstants.oauthCookieIdentifier);
         
         if (oauthBearer) {
-            context.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + oauthBearer
+            context.$axios.defaults.headers.common[constants.systemConstants.oauthHeader] = 'Bearer ' + oauthBearer
         }
         
         if (jwt) {
