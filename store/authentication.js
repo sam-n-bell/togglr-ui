80429ab6dfa2a6700c306d0f6ccee9f0a1dc2e1d
sambell <bell.sam@Outlook.com>
2020-05-03 12:39:42 -0500

    user admin changes

--- store/authentication.js
+++ store/authentication.js
@@ -11,6 +11,7 @@ const state = () => ({
     logoutDialogShowing: false,
     jwt: null,
     user: "",
+    roles: [],
     corpId: 1
 });
 
@@ -22,6 +23,17 @@ const getters = {
         );
 
         return !!state.user || !!token;
+    },
+    isUserSuperAdmin: state => {
+        if (state.roles){
+            let superAdmin = state.roles.filter(
+                role => role.authority.toUpperCase() === 'ROLE_SUPERADMIN'
+            );
+            if (superAdmin.length > 0) {
+                return true;
+            }
+        }
+        return false;
     },
     getUsernameIntial: state => {
         if (state.user) {
@@ -96,6 +108,7 @@ const mutations = {
 
         let decoded = jsonwebtoken.decode(jwt);
         state.user = decoded.details.username;
+        state.roles = decoded.details.authorities
     },
     login(state) {
         state.loginInProgress = true;
